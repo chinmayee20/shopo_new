4b01260fcea7c12a0e62294756aa93eef5648305
Akhil Bolla <akhilbolla89@gmail.com>
2024-02-21 22:18:39 -0800

    ready to review

---
+++ src/components/BreadcrumbCom.jsx
@@ -0,0 +1,21 @@
+
+export default function BreadcrumbCom({
+  paths = [{ name: "home", path: "/" }],
+}) {
+  return (
+    <>
+      {paths && paths.length > 0 && (
+        <div className="breadcrumb-wrapper font-400 text-[13px] text-qblack mb-[23px]">
+          {paths.map((path) => (
+            <span key={path.name}>
+              <a to={path.path}>
+                <span className="mx-1 capitalize">{path.name}</span>
+              </a>
+              <span className="sperator">/</span>
+            </span>
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
